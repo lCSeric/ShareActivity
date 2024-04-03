ae4160c372f67b154b562c3a76a2b37cce93c27c
Eric Lin <tur99299@temple.edu>
2024-04-03 15:29:10 -0400

    First, make the editText view to string ,and then use if-else to make sure editText is not empty.

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,8 +1,10 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,18 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text if not empty
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+            val TextToShare = editText.toString()
+            if (TextToShare.isNotEmpty()){
+                val sentIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, TextToShare)
+                    type = "text/plain"
+                }
+                val shareIntent = Intent.createChooser(sentIntent, null)
+                startActivity(shareIntent)
+        }else{
+            Toast.makeText(this, "It is empty",Toast.LENGTH_SHORT).show()
+            }
 
         }
     }
